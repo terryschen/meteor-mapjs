06b530ce5f5f5566b71915d4d7f60eade0f0861d
Terry Chen <chen@magnetar.arborpoint>
2013-12-23 14:01:30 -0500

    fix package.js

--- package.js
+++ package.js
@@ -5,6 +5,10 @@ Package.describe({
 Package.on_use(function (api) {
   api.export('MAPJS', 'client');
   api.use('underscore', 'client');
+  api.use('jquery', 'client');
+  api.use('jquery-hotkeys', 'client');
+  api.use('jquery-mousewheel', 'client');
+  api.use('hammer', 'client');
   // latest kinetic api has changed so I unfortunately
   // can NOT use the kinetic.js atmosphere package...
   api.add_files('kinetic-v4.5.4.js', 'client');
